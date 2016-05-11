41efa91f22e8c5ba97de2dadacbb80979243f3ba
Stanislav Seletskiy <s.seletskiy@gmail.com>
2016-05-11 20:11:04 +0600

    zsh: file var used by zgen!

--- plugin.zsh
+++ plugin.zsh
@@ -1,3 +1,10 @@
-for file in $(readlink -f "$(dirname "$0")")/zsh/*.zsh; do
-    source "$file"
-done
+:include-deadfiles() {
+    local self=$1
+    local file
+
+    for file in $(readlink -f "$(dirname "$self")")/zsh/*.zsh; do
+        source "$file"
+    done
+}
+
+:include-deadfiles "$0"
