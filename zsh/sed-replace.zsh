14c8182c41c26b3ac4bfba88a85160554b490ac8
Stanislav Seletskiy <s.seletskiy@gmail.com>
2017-03-07 18:39:54 +0700

    sed-replace: ignore dirs & binary, prettier output

--- zsh/sed-replace.zsh
+++ zsh/sed-replace.zsh
@@ -8,10 +8,16 @@ sed-replace() {
         shift
     fi
 
+    local tmpdir=$(mktemp -d)
+
     if [ $# -ne 0 ]; then
         local diff=false
         local files=()
         for file in $@; do
+            if [[ -d "$file" || -x "$file" ]]; then
+                continue
+            fi
+
             if [ "$file" = "!" ]; then
                 diff=true
             else
@@ -21,9 +27,15 @@ sed-replace() {
 
         for file in "${files[@]}"; do
             if $diff; then
-                after=$(mktemp -u)
-                sed -r "s/$from/$to/g" $file > $after
-                git diff --color $file $after | diff-so-fancy
+                after=$tmpdir/$(basename $file)
+                sed -r "s/$from/$to/g" $file >| $after
+                if ! cmp -s $file $after; then
+                    printf "\e[4m%s\e[0m\n" "$file"
+                    diff -u --color=always $file $after \
+                        | diff-so-fancy \
+                        | tail -n+3
+                    printf "\n"
+                fi
             else
                 sed -ri "s/$from/$to/g" $file
             fi
@@ -32,5 +44,7 @@ sed-replace() {
     else
         sed -r "s/$from/$to/g"
     fi
+
+    rm -rf "$tmpdir"
 }
 
